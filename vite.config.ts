0a3307f902e6f4960bc1573b2619755bd7745113
pierreleripoll <pierreripoll13@gmail.com>
2025-02-24 13:24:31 +0100

    feat: added quasar

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,33 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue"; // if using Vue; install with npm install --save-dev @vitejs/plugin-vue
+import path from "path";
+import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
+import { fileURLToPath } from "node:url";
 
 export default defineConfig({
   // adjust root if needed (e.g., to "src" or another folder)
   root: "./vueSrc/",
   publicDir: "public",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "vueSrc"),
+    },
+  },
   build: {
     outDir: "dist",
     // other build options as required
   },
-  plugins: [vue()],
+  plugins: [
+    vue({
+      template: { transformAssetUrls },
+    }),
+
+    // @quasar/plugin-vite options list:
+    // https://github.com/quasarframework/quasar/blob/dev/vite-plugin/index.d.ts
+    quasar({
+      sassVariables: fileURLToPath(
+        new URL("./vueSrc/quasar-variables.sass", import.meta.url)
+      ),
+    }),
+  ],
 });
