9268a8d1aee1002da256ab7a086e01384f84caf2
pierreleripoll <pierreripoll13@gmail.com>
2025-03-03 17:24:22 +0100

    feat: update DownloadDataForm with new input fields and improved download logic

--- vueSrc/components/DownloadDataForm.vue
+++ vueSrc/components/DownloadDataForm.vue
@@ -3,59 +3,70 @@
     <q-card-section>
       <div class="text-h6">Download Data</div>
     </q-card-section>
+    <q-card-section class="q-gutter-md">
+      <h3 class="text-h6">Not yet implemented :</h3>
+      <q-select
+        v-model="level"
+        outlined
+        disable
+        label="Select Level"
+        :options="levelOptions"
+      />
+      <q-select
+        outlined
+        disable
+        label="Type"
+        v-model="type"
+        :options="typeOptions"
+      />
+
+      <q-input
+        outlined
+        disable
+        type="number"
+        label="Density"
+        v-model="density"
+        placeholder="Density (optional)"
+      />
+
+      <q-input
+        outlined
+        disable
+        type="textarea"
+        label="Area (GeoJSON)"
+        v-model="area"
+        placeholder="Enter GeoJSON area..."
+        autogrow
+        rows="4"
+      />
+    </q-card-section>
     <q-card-section>
+      <h3 class="text-h6">Options :</h3>
       <q-form class="q-gutter-md" @submit="downloadData">
-        <q-select
-          v-model="level"
-          outlined
-          disable
-          label="Select Level"
-          :options="levelOptions"
-        />
-        <q-select
-          outlined
-          disable
-          label="Type"
-          v-model="type"
-          :options="typeOptions"
-        />
         <q-select
           outlined
           label="Format"
           v-model="format"
           :options="formatOptions"
         />
-        <q-input
+        <q-select
           outlined
-          disable
-          type="number"
           label="EPSG Code"
+          :options="epsgOptions"
           v-model="epsg"
           placeholder="EPSG Code (optional)"
         />
         <q-input
           outlined
-          disable
           type="number"
-          label="Density"
-          v-model="density"
-          placeholder="Density (optional)"
+          label="Number of points"
+          v-model="number"
+          placeholder="Number of points (optional)"
         />
-        <q-input
-          outlined
-          disable
-          type="textarea"
-          label="Area (GeoJSON)"
-          v-model="area"
-          placeholder="Enter GeoJSON area..."
-          autogrow
-          rows="4"
-        />
+
         <q-btn label="Download" type="submit" color="primary" />
       </q-form>
     </q-card-section>
-
-    <q-card-actions align="right"> </q-card-actions>
   </q-card>
 </template>
 
@@ -69,13 +80,6 @@ const formatOptions = [
   // { label: "LAS v1.3", value: "lasv13" },
   // { label: "LAS v1.2", value: "lasv12" },
 ];
-
-const type = ref("traj");
-const format = ref(formatOptions[0]);
-const epsg = ref("");
-const density = ref("");
-const area = ref("");
-
 const levelOptions = [
   { label: "Raw 3D laser vectors", value: "0" },
   { label: "ALS Mounting and Calibration", value: "1" },
@@ -86,6 +90,15 @@ const levelOptions = [
   { label: "DTM/DSM/CHM", value: "6" },
   { label: "Segmented Point Cloud", value: "7" },
 ];
+
+const epsgOptions = ["EPSG:4326", "EPSG:3857", "EPSG:2056"];
+
+const type = ref("traj");
+const format = ref(null);
+const epsg = ref(null);
+const density = ref("");
+const area = ref("");
+const number = ref(1000);
 
 const level = ref(levelOptions[0]);
 
@@ -109,26 +122,17 @@ async function downloadData() {
   );
 
   // If type is metadata, add removal of all non-geometry attributes
-  if (type.value === "metadata") {
-    params.append("remove_all_attributes", "true");
-  }
-  // Removed type parameter
+  if (type.value === "metadata") params.append("remove_all_attributes", "true");
 
-  if (format.value) {
-    params.append("format", format.value.value);
-  }
-  if (epsg.value) {
-    params.append("outcrs", epsg.value);
-  }
-  if (density.value) {
-    params.append("density", density.value);
-  }
-  if (area.value) {
-    params.append("roi", area.value);
-  }
-  // Replace the "level" parameter with "returns"
-  // params.append("returns", (10).toString());
-  params.append("number", (1).toString());
+  if (format.value) params.append("format", format.value);
+
+  if (epsg.value) params.append("outcrs", epsg.value);
+
+  if (density.value) params.append("density", density.value);
+
+  if (area.value) params.append("roi", area.value);
+
+  if (number.value) params.append("number", number.value.toString());
 
   console.log("Params for download :", params.values(), params.toString());
   const url = prefixPath + `/process-point-cloud?${params.toString()}`;
@@ -136,14 +140,20 @@ async function downloadData() {
 
   try {
     const response = await fetch(url);
+    console.log("Response:", response);
     if (!response.ok) {
       throw new Error(`Server error: ${response.statusText}`);
     }
     const blob = await response.blob();
+    console.log("Blob:", blob);
     const downloadUrl = window.URL.createObjectURL(blob);
     const a = document.createElement("a");
     a.href = downloadUrl;
-    a.download = `${pointcloudId}-processed.${format.value.value}`;
+    const header = response.headers.get("Content-Disposition");
+    const parts = header!.split(";");
+    const filename = parts[1].split("=")[1];
+    a.download = filename || "output_pointcloud";
+
     document.body.appendChild(a);
     a.click();
     a.remove();
